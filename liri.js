9afd2d266c0ef85b62d084342b5d408d4d92ed6b
ronald chaudry <rchaud03@gmail.com>
2018-09-17 18:45:19 -0400

    1845

--- liri.js
+++ liri.js
@@ -4,8 +4,6 @@ var req = require("request");
 var Spotify = require('node-spotify-api');
 
 var keys = require('./keys.js');
-// var spotKeys = require("./keys.js");
-
 
 var searchType = process.argv[2];
 var searchItem = process.argv[3];
@@ -53,12 +51,6 @@ function concert () {
 				"---------------------------------------------");
 
 			})
-
-			// console.log("\n" + "Venue: " + data[0].venue.name + "\n" +
-			//             "City: " + data[0].venue.city + ", " + data[0].venue.region + "\n" +
-			//             "Country: " + data[0].venue.country + "\n" +
-            //             "Date and Time: " + data[0].datetime + "\n" +
-            //             "--------------------------------------------------------------" + "\n");
 		}
 	})
 	
@@ -67,14 +59,7 @@ function concert () {
 //Movie Search
 function movie () {
 	var movieUrl = "https://www.omdbapi.com/?apikey=47a2577f&t=" + searchItem;
-	// if (searchItem = null) {
-	// 	console.log("You didn type a movie name");
 
-	// } else { 
-	// 	moviesearch(searchItem);
-
-	// }
-	// function moviesearch () { 
 		req(movieUrl, function (error, response, body) {
 		var data = JSON.parse(body);
 		if (!error && response.statusCode === 200) {
@@ -83,32 +68,17 @@ function movie () {
 					"\n" +
 					"Title: " + data.Title + "\n" +
 					"Year: " + data.Year + "\n" +
-					"IMDB rating: " + data.Ratings[0].Value + "\n" +
-					"Rotten Tomatoes rating: " + data.Ratings[0].Value +"\n" +
+					"RATINGS: IMDB = " + data.Ratings[0].Value +" " + "Rotten Tomatoes = " + data.Ratings[0].Value +"\n" +
+					// "Rotten Tomatoes rating: " + data.Ratings[0].Value +"\n" +
 					"Languages: " + data.Language + "\n" +
 					"Plot: " + data.Plot + "\n" +
                     "Cast: " + data.Actors + "\n" +
                     "--------------------------------------------" + "\n");
 		}
 	})
-//}
 }
 
 //Song Search
-
-function spotifying () {
-	spotify.search({type: 'track', query: searchItem}, function (err, data) {
-		console.log("We are searching Spotify for song "+ searchItem);
-		if (!err && response.statusCode === 200) {
-
-		// if (!error && response.statusCode === 200) {
-		// 	console.log("An error occured" + error);
-		// }
-		console.log("Done searching Spotify for song "+ searchItem);
-		console.log(data);
-	}
-})
-}
 
 function spotifyingtwo () {  
 		spotify.search({ type: 'track', query: searchItem }, function(err, data) {
